8814798acc76daed193fa4f3a4fdf5922a120db7
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2026-04-08 22:02:11 +0100

    If we don't allow IVs and Level to be modifiable, there is not point in having `Adjust Level = 50`

--- data/mods/champions/scripts.ts
+++ data/mods/champions/scripts.ts
@@ -11,12 +11,10 @@ export const Scripts: ModdedBattleScriptsData = {
 		const tr = this.trunc;
 		let stat = baseStats[statName];
 		const evs = set.evs[statName] ? 4 + 8 * (set.evs[statName] - 1) : 0;
-		const ivs = 31;
-		const level = 50;
 		if (statName === 'hp') {
-			return tr(tr(2 * stat + ivs + tr(evs / 4) + 100) * level / 100 + 10);
+			return tr(tr(2 * stat + set.ivs[statName] + tr(evs / 4) + 100) * set.level / 100 + 10);
 		}
-		stat = tr(tr(2 * stat + ivs + tr(evs / 4)) * level / 100 + 5);
+		stat = tr(tr(2 * stat + set.ivs[statName] + tr(evs / 4)) * set.level / 100 + 5);
 		const nature = this.dex.natures.get(set.nature);
 		// Natures are calculated with 16-bit truncation.
 		// This only affects Eternatus-Eternamax in Pure Hackmons.
