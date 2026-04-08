dc4b238761255c10162906746a7002116cffb7af
andrebastosdias <andrebastosdias@gmail.com>
2026-04-08 19:53:48 +0100

    Fix condition

--- sim/battle-actions.ts
+++ sim/battle-actions.ts
@@ -1824,7 +1824,7 @@ export class BattleActions {
 		// Final modifier. Modifiers that modify damage after min damage check, such as Life Orb.
 		baseDamage = this.battle.runEvent('ModifyDamage', pokemon, target, move, baseDamage);
 
-		if (move.isZOrMaxPowered && target.getMoveHitData(move).brokeProtect) {
+		if (target.getMoveHitData(move).brokeProtect) {
 			baseDamage = this.battle.modify(baseDamage, 0.25);
 			if (move.isZOrMaxPowered) this.battle.add('-zbroken', target);
 		}
