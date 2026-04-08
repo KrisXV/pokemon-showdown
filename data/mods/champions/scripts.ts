ba9b5df17d855d5d9a5461f086e6ea1ee66d6f6e
andrebastosdias <andrebastosdias@gmail.com>
2026-04-08 10:21:47 +0100

    Nerf Unseen Fist

--- data/mods/champions/scripts.ts
+++ data/mods/champions/scripts.ts
@@ -1,3 +1,13 @@
 export const Scripts: ModdedBattleScriptsData = {
 	gen: 9,
+	checkMoveBreaksProtect(move: ActiveMove, attacker: Pokemon, defender: Pokemon, blockStatus = true) {
+		if (move.flags['protect'] && (move.category !== 'Status' || blockStatus)) {
+			return false;
+		}
+		if ((move.isZ || move.isMax || attacker.hasAbility(['piercingdrill', 'unseenfist'])) &&
+			!['gmaxoneblow', 'gmaxrapidflow'].includes(move.id)) {
+			defender.getMoveHitData(move).brokeProtect = true;
+		}
+		return true;
+	},
 };
