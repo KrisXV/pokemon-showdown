05ba889d508563806382f0d8c1e240fdfc80d894
andrebastosdias <andrebastosdias@gmail.com>
2026-04-08 10:49:56 +0100

    Implement move changes

--- data/mods/champions/moves.ts
+++ data/mods/champions/moves.ts
@@ -1,4 +1,33 @@
 export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
+	crabhammer: {
+		inherit: true,
+		accuracy: 95,
+	},
+	direclaw: {
+		inherit: true,
+		flags: { contact: 1, protect: 1, mirror: 1, metronome: 1, slicing: 1 },
+		secondary: {
+			chance: 30,
+			onHit(target, source) {
+				const result = this.random(3);
+				if (result === 0) {
+					target.trySetStatus('psn', source);
+				} else if (result === 1) {
+					target.trySetStatus('par', source);
+				} else {
+					target.trySetStatus('slp', source);
+				}
+			},
+		},
+	},
+	firstimpression: {
+		inherit: true,
+		basePower: 100,
+	},
+	gravapple: {
+		inherit: true,
+		basePower: 90,
+	},
 	ironhead: {
 		inherit: true,
 		secondary: {
@@ -6,8 +35,31 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 			volatileStatus: 'flinch',
 		},
 	},
+	moonblast: {
+		inherit: true,
+		secondary: {
+			chance: 10,
+			boosts: {
+				spa: -1,
+			},
+		},
+	},
 	protect: {
 		inherit: true,
 		pp: 5,
+	},
+	snaptrap: {
+		inherit: true,
+		type: "Steel",
+	},
+	toxicthread: {
+		inherit: true,
+		boosts: {
+			spe: -2,
+		},
+	},
+	tropkick: {
+		inherit: true,
+		basePower: 85,
 	},
 };
