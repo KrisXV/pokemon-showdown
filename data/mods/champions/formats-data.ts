6d21f5f6c85a37ebdc443322f69450606a9cd3ba
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2026-04-08 12:04:03 -0700

    Remove Mega Raichus and Mega Z

--- data/mods/champions/formats-data.ts
+++ data/mods/champions/formats-data.ts
@@ -217,10 +217,12 @@ export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormat
 		tier: "OU",
 	},
 	raichumegax: {
-		tier: "OU",
+		isNonstandard: "Past",
+		tier: "Illegal",
 	},
 	raichumegay: {
-		tier: "OU",
+		isNonstandard: "Past",
+		tier: "Illegal",
 	},
 	sandshrew: {
 		isNonstandard: "Past",
@@ -1979,7 +1981,8 @@ export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormat
 		tier: "OU",
 	},
 	absolmegaz: {
-		tier: "OU",
+		isNonstandard: "Past",
+		tier: "Illegal",
 	},
 	snorunt: {
 		isNonstandard: "Past",
@@ -2346,7 +2349,8 @@ export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormat
 		tier: "OU",
 	},
 	garchompmegaz: {
-		tier: "OU",
+		isNonstandard: "Past",
+		tier: "Illegal",
 	},
 	riolu: {
 		isNonstandard: "Past",
@@ -2359,7 +2363,8 @@ export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormat
 		tier: "OU",
 	},
 	lucariomegaz: {
-		tier: "OU",
+		isNonstandard: "Past",
+		tier: "Illegal",
 	},
 	hippopotas: {
 		isNonstandard: "Past",
